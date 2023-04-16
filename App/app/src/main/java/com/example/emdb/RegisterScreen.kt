b9b6a03c11492bbda6594abac4506096c446d9ba
Carlos Portillo <carloskportillo@gmail.com>
2023-04-16 16:59:11 -0600

    Login Screen Design Finished

--- App/app/src/main/java/com/example/emdb/RegisterScreen.kt
+++ App/app/src/main/java/com/example/emdb/RegisterScreen.kt
@@ -2,13 +2,10 @@ package com.example.emdb
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Window
-import android.view.WindowManager
 
-class LoginScreen : AppCompatActivity() {
+class RegisterScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login_screen)
-        supportActionBar?.hide();
+        setContentView(R.layout.activity_register_screen)
     }
 }
