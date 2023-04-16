a35aa6fd32179eb311cc6a211dca2fa3c662d928
Carlos Portillo <carloskportillo@gmail.com>
2023-04-16 17:32:54 -0600

    Login and Register Screen Design Finished

--- App/app/src/main/java/com/example/emdb/LoginScreen.kt
+++ App/app/src/main/java/com/example/emdb/LoginScreen.kt
@@ -1,14 +1,27 @@
 package com.example.emdb
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Window
 import android.view.WindowManager
+import android.widget.TextView
 
 class LoginScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_screen)
         supportActionBar?.hide();
+
+        val registerTv = findViewById<TextView>(R.id.registerTv);
+        registerTv.setOnClickListener{
+            this.goToRegister();
+        }
+
+    }
+
+    private fun goToRegister(){
+        val intent = Intent(this, RegisterScreen::class.java)
+        startActivity(intent);
     }
 }
--- App/app/src/main/java/com/example/emdb/RegisterScreen.kt
+++ App/app/src/main/java/com/example/emdb/RegisterScreen.kt
@@ -1,11 +1,24 @@
 package com.example.emdb
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class RegisterScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register_screen)
+        supportActionBar?.hide();
+
+        val loginTv = findViewById<TextView>(R.id.loginTv);
+        loginTv.setOnClickListener{
+            this.goToLogin();
+        }
+    }
+
+    private fun goToLogin(){
+        val intent = Intent(this, LoginScreen::class.java);
+        startActivity(intent);
     }
 }
