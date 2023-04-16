24268cade574739b54e5b33fb8991152f35073b1
Alemau97 <mauriciomn@hotmail.es>
2023-04-16 17:17:22 -0600

    Multilenguaje y headers Movies and Series

---
+++ App/app/src/main/java/com/example/emdb/Peliculas.kt
@@ -0,0 +1,11 @@
+package com.example.emdb
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Peliculas : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_peliculas)
+    }
+}
