757019b8b0ac8d6324a4bc7d98e5f14acbb72032
Alejandro Pacheco <pacheco29alejandro@gmail.com>
2023-05-14 14:40:24 -0600

    Utilizacion de API

---
+++ ImdbApiRequest/app/src/main/java/com/example/imdbapirequest/ApiService.kt
@@ -0,0 +1,7 @@
+package com.example.imdbapirequest
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
---
+++ ImdbApiRequest/app/src/main/java/com/example/imdbapirequest/Movies.kt
@@ -0,0 +1,15 @@
+package com.example.imdbapirequest
+
+import android.util.Log
+import android.widget.ImageView
+import android.widget.TextView
+import com.squareup.picasso.Picasso
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class Movies {
+
+
+}
+
